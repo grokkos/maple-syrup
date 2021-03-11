d05b9526b9a861c2e9938111a6800c95d06720fa
George <gerokkos@gmail.com>
2021-03-11 18:58:19 +0200

    add batchid fk on transaction instead of userid

--- api/models/Transaction.go
+++ api/models/Transaction.go
@@ -5,9 +5,9 @@ import (
 )
 
 type Transaction struct {
-	ID                uint64 `gorm:"primary_key;auto_increment" json:"id"`
-	Amount            int    `gorm:"not null" json:"amount"`
-	TransactionUserID uint32 `sql:"type:int REFERENCES users(id)" json:"transaction_user_id"`
+	ID                 uint64 `gorm:"primary_key;auto_increment" json:"id"`
+	Amount             int    `gorm:"not null" json:"amount"`
+	TransactionBatchID uint32 `sql:"type:int REFERENCES batches(id)" json:"transaction_batch_id"`
 }
 
 func (t *Transaction) FindAllTransactions(db *gorm.DB) (*[]Transaction, error) {
